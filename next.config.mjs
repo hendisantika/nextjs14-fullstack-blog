43f894ddfb749f002859d0f90a9836c8b793f74b
hendisantika <hendisantika@yahoo.co.id>
2024-07-29 07:31:43 +0700

    Add required configs :two:🫸🌀✏️📗🐧🐳

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn2.iconfinder.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'plus.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
